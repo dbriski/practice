cc40d6ce7639c074471b5987bb75e85666ae48f9
dbriski <dbriski26@gmail.com>
2022-01-24 11:43:28 +0100

    Added from String:replace() to NOR tasks

--- Tasks/JS_HERO /scripts/README.md
+++ Tasks/JS_HERO /scripts/README.md
@@ -39,7 +39,68 @@ returning the first word in that string.
 The first word are all characters up to the first space.
 Example: firstWord('see and stop') should return 'see'.
 
-### 
+### String: replace()
 Write a function normalize, that replaces '-' with '/' in a date string.
 Note: chaining.
 Example: normalize('20-05-2017') should return '20/05/2017'.
+
+### Numbers
+Write a function add that takes two numbers and returns their sum.
+Example: add(1, 2) should return 3.
+
+### Fahrenheit
+Write a function toFahrenheit that converts 
+a temperature from Celsius to Fahrenheit.
+Example: toFahrenheit(0) should return 32.
+
+### Modulo
+Write a function onesDigit that takes a natural number 
+and returns the ones digit of that number.
+Example: onesDigit(2674) should return 4.
+
+### Parentheses
+Write a function mean that takes 2 numbers and returns their mean value.
+Example: mean(1, 2) should return 1.5.
+
+### Math
+Write a function hypotenuse that calculates the length of the hypotenuse of a right triangle. 
+The length of the two legs is passed to the function. 
+Tip: In a right triangle the Pythagorean theorem is valid. 
+If a and b are the lengths of the two legs and c is the length of the hypotenuse, 
+the following is true: a² + b² = c². 
+Since 3² + 4² = 5² applies, hypotenuse(3, 4) should return 5.
+
+### min and max
+Write a function midrange, that calculates the midrange of 3 numbers. 
+The midrange is the mean of the smallest and largest number.
+Example: midrange(3, 9, 1) should return (9+1)/2 = 5.
+
+### Math.PI
+Write a function area that calculates the area of a circle. 
+The function is given the radius of the circle.
+Example: area(1) should return π and area(2) should return 4 * π.
+
+### Rounding
+Write a function round100 that rounds a number to the nearest hundred.
+Example: round100(1749) should return 1700 and round100(856.12) should return 900.
+
+### Random numbers
+Write a function dice that returns like a dice a random number between 1 and 6.
+
+### parseInt
+Write a function add that takes a string with a summation task and returns its result as a number. 
+Two natural numbers should be added. The summation task is a string of the form '102+17'.
+Example: add('102+17') should return 119.
+
+### Boolean
+Write a function nand that takes two Boolean values. If both values are true, the result should be false. 
+In the other cases the return should be true.
+I.e.: The call nand(true, true) should return false. The calls nand(true, false), 
+nand(false, true) and nand(false, false) should return true.
+
+### NOR
+Write a function nor that takes two Boolean values. 
+If both values are false, the result should be true. 
+In the other cases the return should be false.
+I.e.: The call nor(false, false) should return true. 
+The calls nor(true, false), nor(false, true) and nor(true, true) should return false.
--- Tasks/JS_HERO /scripts/app.js
+++ Tasks/JS_HERO /scripts/app.js
@@ -1,40 +1,40 @@
 // String: length
 function length(name) {
   return name.length;
- }
- length('bla');
+}
+length('bla');
 
- //String: toUpperCase()
- function toCase(input) {
+//String: toUpperCase()
+function toCase(input) {
   return input.toLowerCase() + '-' + input.toUpperCase();
- }
- toCase('bla');
+}
+toCase('bla');
 
 // String: charAt()
 function shortcut(name1, name2) {
   let char1 = name1.charAt(0);
   let char2 = name2.charAt(0);
   return char1 + char2;
- }
- shortcut('Lala', 'Land');
+}
+shortcut('Lala', 'Land');
 
 //String: trim()
 function firstChar(name) {
   let trim = name.trim();
   return trim.charAt(0);
- }
- firstChar(' Lisa Lisa ');
+}
+firstChar(' Lisa Lisa ');
 
 // String: indexOf()
 function indexOfIgnoreCase(name1, name2) {
   let lowChar1 = name1.toLowerCase();
   let lowChar2 = name2.toLowerCase();
   return lowChar1.indexOf(lowChar2);
- }
- indexOfIgnoreCase('bit', 'IT');
+}
+indexOfIgnoreCase('bit', 'IT');
 
 // String: indexOf() with from index
-function secondIndexOf(name1, name2){
+function secondIndexOf(name1, name2) {
   let n1 = name1.indexOf(name2);
   return name1.indexOf(name2, n1 + 1);
 }
@@ -44,11 +44,118 @@ secondIndexOf('White Rabbit', 'it');
 function firstWord(name) {
   let firstBlank = name.indexOf(' ');
   return name.substr(0, firstBlank);
- }
- firstWord('Dani je cool');
+}
+firstWord('Dani je cool');
 
 //String: replace() - NOTE: chaining!
 function normalize(date) {
   return date.replace('-', '/').replace('-', '/');
+}
+normalize('19-01-2021');
+
+// Numbers
+function add(num1, num2) {
+  return num1 + num2;
+}
+add(1, 3);
+
+// Fahrenheit
+function toFahrenheit(input) {
+  return 1.8 * input + 32;
+}
+toFahrenheit(2);
+
+// Modulo
+function onesDigit(number) {
+  return number % 10;
+}
+onesDigit(62);
+
+// Parentheses
+function mean(num1, num2) {
+  let sum = num1 + num2;
+  return sum / 2;
+}
+mean(1, 2);
+
+// Math
+//My example
+function hypotenuse(a, b) {
+  return Math.sqrt(a * a + b * b);
+}
+hypotenuse(3, 4);
+//Their solution
+function hypotenuse(a, b) {
+  let cSquare = Math.pow(a, 2) + Math.pow(b, 2);
+  return Math.sqrt(cSquare);
+}
+
+// min and max
+//My example
+function midrange(n1,n2,n3) {
+  return (Math.min(n1,n2,n3) + Math.max(n1,n2,n3)) / 2;
+}
+midrange(2,7,4);
+//Their solution
+function midrange(a, b, c) {
+  let min = Math.min(a, b, c);
+  let max = Math.max(a, b, c);
+  return (min + max) / 2;
+}
+
+// Math.PI
+function area(n1) {
+  return Math.PI * (n1 * n1) ;
  }
- normalize('19-01-2021');
+ area(4)
+
+// Rounding
+function round100(num) {
+  return Math.round(num/100)*100;
+}
+round100(1230);
+
+// Random numbers
+function dice() {
+  return Math.floor(Math.random() * (7 - 1)) + 1; 
+} 
+dice()
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+// parseInt
+// My example
+function add(textnum) {
+  const operatorPosition = textnum.indexOf('+');
+  const num1 = textnum.substr(0, operatorPosition);
+  const firstNum = parseInt(num1);
+  const num2 = textnum.substr(operatorPosition)  
+  const secondNum = parseInt(num2)
+  return firstNum + secondNum;
+}
+add('100+300');
+//Their solution
+function add(s) {
+  let summand1 = parseInt(s, 10);
+  let indexPlus = s.indexOf('+');
+  let sAfterPlus = s.substr(indexPlus + 1);
+  let summand2 = parseInt(sAfterPlus, 10);
+  return summand1 + summand2;
+}
+add('100+300');
+
+// Boolean
+function nand(value1, value2) {
+  let values = value1 && value2;
+  return !values;
+}
+nand(true, true)
+
+// NOR
+function nor(v1, v2) {
+  const values = v1 || v2;
+  return !values;
+}
+nor(false, false);
